3c394216b91b32a50f4054481807f4867d9af4f6
karl521hk <1733762515@qq.com>
2019-08-26 01:35:49 +0800

    三小时入门django框架，完成3-6

---
+++ django_introduction/blog/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path, include
+
+import blog.views
+
+
+urlpatterns = [
+    path('hello_world', blog.views.hello_world),
+    path('content', blog.views.article_content)
+]
+
+
